e91b50166611a1ee8e55a06a6b6990b488944959
Juan Segura <jsegura@cipfpbatoi.es>
2019-03-20 10:59:38 +0100

    Update script_reiniciar.usuari.sh

--- proyectos/manteniment/script_reiniciar.usuari.sh
+++ proyectos/manteniment/script_reiniciar.usuari.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script per a eliminar TOTES les DADES dels equips dels alumnes
 
-echo ATENCIÓ: aquest script elimina TOTES les dades de l'usuari indicat en els equips dels alumnes. Per a continu$
+echo ATENCIÓ: aquest script elimina TOTES les dades de l\'usuari indicat en els equips dels alumnes. Per a continu$
 read confirm
 if [ "$confirm" != "CONTINUAR" ]
 then
@@ -17,6 +17,7 @@ for ipn in 11 12 13 14 15 16 21 22 23 24 25 26 31 32 33 34 35 36 41 42 43 44 45 
 do
         ip="172.16."$aula"."$ipn
 
+echo Borrando PC $ip
 ssh $ip "deluser $usuari"
 ssh $ip "rm -R /home/$usuari"
 ssh $ip "useradd -s /bin/bash $usuari"
