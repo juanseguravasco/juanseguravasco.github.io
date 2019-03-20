9fa629b0c0b489d4c5a1de530f08323ef5596934
Juan Segura <jsegura@cipfpbatoi.es>
2019-03-20 10:50:54 +0100

    Update script_reiniciar.usuari.sh

--- proyectos/manteniment/script_reiniciar.usuari.sh
+++ proyectos/manteniment/script_reiniciar.usuari.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Script per a fer a un usuari sudoer en el seu equip
+# Script per a eliminar TOTES les DADES dels equips dels alumnes
 
+echo ATENCIÓ: aquest script elimina TOTES les dades de l'usuari indicat en els equips dels alumnes. Per a continu$
+read confirm
+if [ "$confirm" != "CONTINUAR" ]
+then
+        echo Procés cancel·lat
+        return
+fi
 echo Escriu el num d\'aula
 read aula
 echo Escriu el nom de l\'usuari que vols eliminar
@@ -8,12 +15,12 @@ read usuari
 
 for ipn in 11 12 13 14 15 16 21 22 23 24 25 26 31 32 33 34 35 36 41 42 43 44 45 46 51
 do
-	ip="172.16."$aula"."$ipn
+        ip="172.16."$aula"."$ipn
 
 ssh $ip "deluser $usuari"
 ssh $ip "rm -R /home/$usuari"
 ssh $ip "useradd -s /bin/bash $usuari"
-ssh $ip "echo $usuari:contrasenya | chpasswd"
+ssh $ip "echo $usuari:$usuari12 | chpasswd"
 ssh $ip "cp /etc/skel/.* /home/$usuari"
 ssh $ip "cp -R /etc/skel/.config /home/$usuari"
 ssh $ip "cp -R /etc/skel/.mozilla /home/$usuari"
